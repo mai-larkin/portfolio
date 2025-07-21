8dfa6ad34d22dae4bcaa98e3181cb8d0c930c26e
Mai Larkin <mailarkin1@gmail.com>
2025-07-21 17:25:59 +1200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -5,21 +5,21 @@ import CodePanel from './components/CodePanel';
 export default function Home() {
   return (
     <>
-      <div className="container max-w-5xl pl-7 sm:pl-23 pr-7 sm:pr-22 font-[family-name:var(--font-geist-sans)]">
+      <div className="container max-w-5xl md:max-w-6xl pl-7 sm:pl-20 xl:pl-50 pr-7 sm:pr-20 font-[family-name:var(--font-geist-sans)]">
       
         <div className="absolute top-30 right-30 lg:w-[600px] lg:h-[600px] w-[300px] h-[300px] bg-violet-700/20 rounded-full blur-3xl"></div>
         <div className="absolute top-40 right-40 lg:w-[400px] lg:h-[400px] w-[150px] h-[150px] bg-violet-500/30 rounded-full blur-2xl"></div>
         <div className="absolute top-30 right-30 lg:w-[300px] lg:h-[300px] w-[100px] h-[100px] bg-violet-400/30 rounded-full blur-xl"></div>
         
         <div className="relative mt-25 sm:mt-45">
-          <h1 className="md:text-5xl sm:text-3xl text-2xl font-bold tracking-light">
+          <h1 className="lg:text-[3.5rem] md:text-5xl sm:text-3xl text-2xl font-bold tracking-light">
             Hi!<span className="ml-1.5"/> I{"'"}m <span className="text-indigo-600 brightness-160">Mai</span>
-            <div className="block h-[1.5em] mt-2">
+            <div className="block h-[1.5em] mt-3">
               <TypewriterText />
             </div>
           </h1>
 
-          <p className="mt-6 text-base sm:text-xl text-gray-300 leading-8">
+          <p className="mt-5 text-base sm:text-xl lg:text-2xl text-gray-300 leading-5.5 sm:leading-8">
             Welcome to my portfolio! I am a second year CompSci student, minoring in both AI and cybersecurity at Victoria University of Wellington.
           </p>
           <div className="flex mt-10 gap-7 text-base sm:text-lg">
